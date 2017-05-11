bcc06af2f99a5882d52dd87e9db75eba92a8150e
Matthew Beevor <mbeevor@gmail.com>
2017-05-11 22:15:00 +0100

    mediaPlayer (play)

--- app/src/main/java/com/example/android/musicalstructure/PlayingActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/PlayingActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.media.MediaPlayer;
 
 /**
  * Java for Now Playing activity
  */
 
 public class PlayingActivity extends AppCompatActivity {
+
+    private MediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +25,21 @@ public class PlayingActivity extends AppCompatActivity {
 
         // Set IDs for each onClickListener //
 
+        ImageView play = (ImageView) findViewById(R.id.play);
         ImageView nowPlaying = (ImageView) findViewById(R.id.now_playing_icon);
         ImageView recentPlay = (ImageView) findViewById(R.id.recent_icon);
         ImageView library = (ImageView) findViewById(R.id.library_icon);
         ImageView store = (ImageView) findViewById(R.id.store_icon);
+
+
+        // Media Player controls
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.imalive);
+                mediaPlayer.start();
+            }
+        });
 
         // Set click listeners for icon tray //
         nowPlaying.setOnClickListener(new View.OnClickListener() {
